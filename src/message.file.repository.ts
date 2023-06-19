c81c6b4b92bd2c1ed07556da9be45dbd1688a93d
nicdo77 <nicolas.daudin@gmail.com>
2023-06-19 13:03:50 +0200

    video "Test d'intégration du FileSystemMessageRepository"

--- src/message.file.repository.ts
+++ src/message.file.repository.ts
@@ -1,19 +1,14 @@
 import * as fs from 'fs';
 import * as path from 'path';
 import { MessageRepository } from "./message.repository";
-import { Message } from './message';
+import { Message, MessageText } from './message';
 export class FileMessageRepository implements MessageRepository {
 
-  private readonly filePath = path.join(__dirname, 'crafty.json');
+  constructor(private readonly filePath = path.join(__dirname, 'crafty.json')) { }
 
   async save(_msg: Message): Promise<void> {
 
-    let messages: Message[] = [];
-    try {
-      const data = await fs.promises.readFile(this.filePath, 'utf-8');
-      messages = transformParsedContentIntoMessages(JSON.parse(data))
-    } catch {
-    }
+    let messages = await this.getMessages();
 
     const existingMessageIndex = messages.findIndex(message => message.id === _msg.id);
     if (existingMessageIndex > -1) {
@@ -22,24 +17,37 @@ export class FileMessageRepository implements MessageRepository {
       messages.push(_msg);
     }
 
-    return fs.promises.writeFile(this.filePath, JSON.stringify(messages));
+    return fs.promises.writeFile(this.filePath, JSON.stringify(messages.map(m => ({
+      id: m.id,
+      author: m.author,
+      text: m.text.value,
+      publishedAt: m.publishedAt
+    }))));
   }
-  async read(): Promise<Message[]> {
-    const data = await fs.promises.readFile(this.filePath, 'utf-8')
-    return transformParsedContentIntoMessages(JSON.parse(data))
 
-  }
+
 
   async getById(id: string): Promise<Message> {
-    const messages = await this.read();
+    const messages = await this.getMessages();
     return messages.find(message => message.id === id)!
   }
 
   async getAllOfUser(user: string): Promise<Message[]> {
-    return (await this.read()).filter(m => m.author === user)
+    const messages = await this.getMessages();
+    return messages.filter(m => m.author === user)
+  }
+
+  private async getMessages(): Promise<Message[]> {
+    const data = await fs.promises.readFile(this.filePath, 'utf-8')
+    const messages = JSON.parse(data.toString()) as { id: string, author: string, text: string, publishedAt: string }[];
+
+    return messages.map((message) => ({
+      id: message.id,
+      author: message.author,
+      text: MessageText.of(message.text),
+      publishedAt: new Date(message.publishedAt)
+    }))
+
   }
 }
 
-const transformParsedContentIntoMessages = (parsed: any[]): Message[] => {
-  return parsed.map<Message>(({ id, author, text, publishedAt }) => ({ author, text, id, publishedAt: new Date(publishedAt) }))
-}
---
+++ src/tests/message/message.file.repository.integration.test.ts
@@ -0,0 +1,134 @@
+import * as path from "path";
+import { FileMessageRepository } from "../../message.file.repository";
+import { messageBuilder } from "./message.builder";
+import * as fs from 'fs';
+
+describe("FileMessageRepository", () => {
+
+  const testFilePath = path.join(__dirname, 'message-test.json');
+
+  beforeEach(async () => {
+    await fs.promises.writeFile(testFilePath, JSON.stringify([]));
+  })
+
+  test("save() can save a message in the filesystem", async () => {
+    const fileRepo = new FileMessageRepository(testFilePath);
+
+    await fileRepo.save(
+      messageBuilder()
+        .withId('message-id-1')
+        .authoredBy('Alice')
+        .withText('Hello there')
+        .publishedAt(new Date('2023-06-19T12:33:00Z'))
+        .build());
+
+    const messagesData = await fs.promises.readFile(testFilePath);
+    const messagesJSON = JSON.parse(messagesData.toString());
+
+    expect(messagesJSON).toEqual([{
+      id: 'message-id-1',
+      author: 'Alice',
+      text: 'Hello there',
+      publishedAt: '2023-06-19T12:33:00.000Z'
+    }])
+  })
+
+  test("save() can edit an existing a message in the filesystem", async () => {
+
+    await fs.promises.writeFile(testFilePath, JSON.stringify([{
+      id: 'message-id-1',
+      author: 'Alice',
+      text: 'Hello there',
+      publishedAt: '2023-06-19T12:33:00.000Z'
+    }]));
+
+    const fileRepo = new FileMessageRepository(testFilePath);
+
+    await fileRepo.save(
+      messageBuilder()
+        .withId('message-id-1')
+        .authoredBy('Alice')
+        .withText('Hello there edited')
+        .publishedAt(new Date('2023-06-19T12:33:00Z'))
+        .build());
+
+    const messagesData = await fs.promises.readFile(testFilePath);
+    const messagesJSON = JSON.parse(messagesData.toString());
+
+    expect(messagesJSON).toEqual([{
+      id: 'message-id-1',
+      author: 'Alice',
+      text: 'Hello there edited',
+      publishedAt: '2023-06-19T12:33:00.000Z'
+    }])
+  })
+
+  test("getById() returns a message by its id", async () => {
+    await fs.promises.writeFile(testFilePath, JSON.stringify([{
+      id: 'message-id-1',
+      author: 'Alice',
+      text: 'Hello there',
+      publishedAt: '2023-06-19T12:37:00.000Z'
+    }, {
+      id: 'message-id-2',
+      author: 'Bob',
+      text: `Hi! I'm Bob and you`,
+      publishedAt: '2023-06-19T12:37:00.000Z'
+    }]));
+
+    const fileRepo = new FileMessageRepository(testFilePath);
+
+    const message = await fileRepo.getById('message-id-1');
+
+
+    expect(message).toEqual(
+      messageBuilder()
+        .withId('message-id-1')
+        .authoredBy('Alice')
+        .withText('Hello there')
+        .publishedAt(new Date('2023-06-19T12:37:00.000Z'))
+        .build()
+    )
+  });
+
+  test("getAllOfUser() only returns messages of that user", async () => {
+    await fs.promises.writeFile(testFilePath, JSON.stringify([{
+      id: 'message-id-1',
+      author: 'Alice',
+      text: 'Hello there',
+      publishedAt: '2023-06-19T12:37:00.000Z'
+    }, {
+      id: 'message-id-2',
+      author: 'Bob',
+      text: `Hi! I'm Bob and you`,
+      publishedAt: '2023-06-19T12:37:10.000Z'
+    },
+    {
+      id: 'message-id-3',
+      author: 'Alice',
+      text: `Well my name is Alice`,
+      publishedAt: '2023-06-19T12:37:20.000Z'
+    }]));
+
+    const fileRepo = new FileMessageRepository(testFilePath);
+
+    const messages = await fileRepo.getAllOfUser('Alice');
+
+
+    expect(messages).toEqual([
+      messageBuilder()
+        .withId('message-id-1')
+        .authoredBy('Alice')
+        .withText('Hello there')
+        .publishedAt(new Date('2023-06-19T12:37:00.000Z'))
+        .build()
+      ,
+      messageBuilder()
+        .withId('message-id-3')
+        .authoredBy('Alice')
+        .withText('Well my name is Alice')
+        .publishedAt(new Date('2023-06-19T12:37:20.000Z'))
+        .build()]
+    );
+  });
+})
