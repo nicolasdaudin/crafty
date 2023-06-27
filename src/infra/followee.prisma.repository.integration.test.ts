5a2b81d1d2a075a71af6986b94daa34c760f275b
nicdo77 <nicolas.daudin@gmail.com>
2023-06-27 12:50:32 +0200

    Vidéo "Test d'intégration avec TestContainers"

---
+++ src/infra/followee.prisma.repository.integration.test.ts
@@ -0,0 +1,150 @@
+import { PrismaClient } from "@prisma/client";
+import { exec } from "child_process";
+import { PostgreSqlContainer, StartedPostgreSqlContainer } from "testcontainers";
+import { promisify } from "util";
+import { PrismaFolloweeRepository } from "./followee.prisma.repository";
+import { Followee } from "../followee/followee";
+import { NotFoundError } from "@prisma/client/runtime";
+
+const asyncExec = promisify(exec);
+
+jest.setTimeout(15000);
+
+
+describe('PrismaFolloweeRepository', () => {
+  let container: StartedPostgreSqlContainer;
+  let prismaClient: PrismaClient;
+
+  beforeAll(async () => {
+    container = await new PostgreSqlContainer()
+      .withDatabase('crafty-test')
+      .withUsername('crafty-test')
+      .withPassword('crafty-test')
+      .withExposedPorts(5432)
+      .start();
+
+    const databaseUrl = `postgresql://crafty-test:crafty-test@${container.getHost()}:${container.getMappedPort(5432)}/crafty-test?schema=public`
+
+    prismaClient = new PrismaClient({
+      datasources: {
+        db: {
+          url: databaseUrl
+        }
+      }
+    })
+    await asyncExec(`DATABASE_URL=${databaseUrl} npx prisma migrate deploy`);
+
+    return prismaClient.$connect()
+
+
+
+  });
+
+  afterAll(async () => {
+    await container.stop({ timeout: 1000 });
+    return prismaClient.$disconnect();
+  })
+
+  beforeEach(async () => {
+    await prismaClient.message.deleteMany();
+    await prismaClient.$executeRawUnsafe('DELETE FROM "User" CASCADE');
+  })
+
+  test('saveFollowee() saves a new followee to a non-existent user', async () => {
+    const followeeRepository = new PrismaFolloweeRepository(prismaClient);
+
+    const newFollowee: Followee = { user: 'Alice', followee: 'Bob' };
+
+    await followeeRepository.saveFollowee(newFollowee);
+
+    const followees = await prismaClient.user.findFirstOrThrow({
+      where: { name: 'Alice' },
+      include: { following: true }
+    });
+
+    expect(followees).toEqual(expect.objectContaining(
+      {
+        name: 'Alice',
+        following: [expect.objectContaining({ name: 'Bob' })]
+      })
+    )
+
+  });
+
+  test('saveFollowee() saves a new followee to an already existing user', async () => {
+    const followeeRepository = new PrismaFolloweeRepository(prismaClient);
+    await prismaClient.user.create({
+      data: {
+        name: 'Alice',
+        following: {
+          create: {
+            name: 'Charlie'
+          }
+        }
+      }
+    });
+
+
+    const newFollowee: Followee = { user: 'Alice', followee: 'Bob' };
+
+    await followeeRepository.saveFollowee(newFollowee);
+
+    const followees = await prismaClient.user.findFirstOrThrow({
+      where: { name: 'Alice' },
+      include: { following: true }
+    });
+    expect(followees).toEqual(expect.objectContaining(
+      {
+        name: 'Alice',
+        following: expect.arrayContaining([
+          expect.objectContaining({ name: 'Bob' }),
+          expect.objectContaining({ name: 'Charlie' })
+        ])
+      })
+    )
+  });
+
+  test('getFolloweesOf() returns a list of followees for that user', async () => {
+    const followeeRepository = new PrismaFolloweeRepository(prismaClient);
+    await prismaClient.user.create({
+      data: {
+        name: 'Alice',
+        following: {
+          create: {
+            name: 'Charlie'
+          }
+        }
+      }
+    });
+
+    const actualFollowees = await followeeRepository.getFolloweesOf('Alice');
+
+    expect(actualFollowees).toEqual(['Charlie']);
+  });
+
+  test('getFolloweesOf() returns an empty array of followees if there are no followees for that user', async () => {
+    const followeeRepository = new PrismaFolloweeRepository(prismaClient);
+    await prismaClient.user.create({
+      data: {
+        name: 'Alice'
+      }
+    });
+
+    const actualFollowees = await followeeRepository.getFolloweesOf('Alice');
+
+    expect(actualFollowees).toEqual([]);
+  })
+
+  test('getFolloweesOf() returns an empty list of followees if the user does not exist', async () => {
+    const followeeRepository = new PrismaFolloweeRepository(prismaClient);
+    try {
+      expect.assertions(1);
+      const actualFollowees = await followeeRepository.getFolloweesOf('Alice');
+
+    } catch (error) {
+
+      expect(error.name).toEqual('NotFoundError');
+    }
+  })
+
+})
---
+++ src/infra/message.prisma.repository.integration.test.ts
@@ -0,0 +1,286 @@
+import { PrismaClient } from '@prisma/client';
+import { PostgreSqlContainer, StartedPostgreSqlContainer } from 'testcontainers';
+import { exec } from 'child_process';
+import { promisify } from 'util';
+import { messageBuilder } from '../messaging/tests/message.builder';
+import { PrismaMessageRepository } from './message.prisma.repository';
+import { Message } from '../messaging/domain/message';
+
+const asyncExec = promisify(exec);
+
+jest.setTimeout(15000);
+
+describe("PrismaMessageRepository", () => {
+  let container: StartedPostgreSqlContainer;
+  let prismaClient: PrismaClient;
+
+
+
+  beforeAll(async () => {
+    container = await new PostgreSqlContainer()
+      .withDatabase('crafty-test')
+      .withUsername('crafty-test')
+      .withPassword('crafty-test')
+      .withExposedPorts(5432)
+      .start()
+
+    const databaseUrl = `postgresql://crafty-test:crafty-test@${container.getHost()}:${container.getMappedPort(5432)}/crafty-test?schema=public`;
+
+    prismaClient = new PrismaClient({
+      datasources: {
+        db: {
+          url: databaseUrl
+        }
+      }
+    })
+
+    await asyncExec(`DATABASE_URL=${databaseUrl} npx prisma migrate deploy`);
+
+    return prismaClient.$connect();
+
+  })
+
+  afterAll(async () => {
+    await container.stop({ timeout: 1000 });
+    return prismaClient.$disconnect();
+
+  })
+
+  beforeEach(async () => {
+    await prismaClient.message.deleteMany();
+    await prismaClient.$executeRawUnsafe('DELETE FROM "User" CASCADE');
+  })
+
+  test('save() saves a new message correctly when there are NO other messages in the database', async () => {
+    const messageRepository = new PrismaMessageRepository(prismaClient);
+
+    const newMessage = messageBuilder()
+      .withId('3')
+      .withText('Well I am Bob')
+      .authoredBy('Bob')
+      .publishedAt(new Date('2023-06-27T08:32:00.000Z'))
+      .build();
+
+
+    await messageRepository.save(newMessage);
+
+    const actual = await prismaClient.message.findUniqueOrThrow({ where: { id: '3' } });
+
+
+    expect(actual).toEqual({
+      id: '3',
+      text: 'Well I am Bob',
+      authorId: 'Bob',
+      publishedAt: new Date('2023-06-27T08:32:00.000Z')
+    })
+  })
+
+  test('save() saves a new message correctly when there are other messages in the database', async () => {
+    const messageRepository = new PrismaMessageRepository(prismaClient);
+
+    const startingMessages = [
+      messageBuilder()
+        .withId('test1')
+        .withText('hi')
+        .authoredBy('Alice')
+        .publishedAt(new Date('2023-06-27T08:30:00.000Z'))
+        .build(),
+      messageBuilder()
+        .withId('test2')
+        .withText('my name is Alice')
+        .authoredBy('Alice')
+        .publishedAt(new Date('2023-06-27T08:31:00.000Z'))
+        .build()
+    ]
+
+    await prismaClient.user.create({ data: { name: 'Alice' } })
+
+    for (const msg of startingMessages) {
+      const messageData = msg.data;
+      await prismaClient.message.create({
+        data: {
+          id: messageData.id,
+          authorId: messageData.author,
+          text: messageData.text,
+          publishedAt: messageData.publishedAt
+        }
+      })
+    }
+
+    const newMessage = messageBuilder()
+      .withId('3')
+      .withText('Well I am Bob')
+      .authoredBy('Bob')
+      .publishedAt(new Date('2023-06-27T08:32:00.000Z'))
+      .build();
+
+
+    await messageRepository.save(newMessage);
+
+    const actual = await prismaClient.message.findUniqueOrThrow({ where: { id: '3' } });
+
+    expect(actual).toEqual({
+      id: '3',
+      text: 'Well I am Bob',
+      authorId: 'Bob',
+      publishedAt: new Date('2023-06-27T08:32:00.000Z')
+    })
+  })
+
+  test('save() edits a message correctly', async () => {
+    const messageRepository = new PrismaMessageRepository(prismaClient);
+
+    const startingMessages = [
+      messageBuilder()
+        .withId('test1')
+        .withText('hi')
+        .authoredBy('Alice')
+        .publishedAt(new Date('2023-06-27T08:30:00.000Z'))
+        .build(),
+      messageBuilder()
+        .withId('test2')
+        .withText('my name is Alice')
+        .authoredBy('Alice')
+        .publishedAt(new Date('2023-06-27T08:31:00.000Z'))
+        .build()
+    ]
+
+    await prismaClient.user.create({ data: { name: 'Alice' } })
+
+    for (const msg of startingMessages) {
+      const messageData = msg.data;
+      await prismaClient.message.create({
+        data: {
+          id: messageData.id,
+          authorId: messageData.author,
+          text: messageData.text,
+          publishedAt: messageData.publishedAt
+        }
+      })
+    }
+
+    const editMessage = messageBuilder()
+      .withId('test2')
+      .withText('my first name is Alice')
+      .authoredBy('Alice')
+      .publishedAt(new Date('2023-06-27T08:31:00.000Z'))
+      .build()
+
+
+    await messageRepository.save(editMessage);
+
+    const actual = await prismaClient.message.findUniqueOrThrow({ where: { id: 'test2' } });
+
+
+    expect(actual).toEqual({
+      id: 'test2',
+      text: 'my first name is Alice',
+      authorId: 'Alice',
+      publishedAt: new Date('2023-06-27T08:31:00.000Z')
+    })
+  })
+
+  test("getById() returns the message by id", async () => {
+    const messageRepository = new PrismaMessageRepository(prismaClient);
+
+    const startingMessages = [
+      messageBuilder()
+        .withId('test1')
+        .withText('hi')
+        .authoredBy('Alice')
+        .publishedAt(new Date('2023-06-27T08:30:00.000Z'))
+        .build(),
+      messageBuilder()
+        .withId('test2')
+        .withText('my name is Alice')
+        .authoredBy('Alice')
+        .publishedAt(new Date('2023-06-27T08:31:00.000Z'))
+        .build()
+    ]
+
+    await prismaClient.user.create({ data: { name: 'Alice' } })
+
+    for (const msg of startingMessages) {
+      const messageData = msg.data;
+      await prismaClient.message.create({
+        data: {
+          id: messageData.id,
+          authorId: messageData.author,
+          text: messageData.text,
+          publishedAt: messageData.publishedAt
+        }
+      })
+    }
+
+    const aliceMessage = await messageRepository.getById('test1');
+
+    expect(aliceMessage).toEqual(messageBuilder()
+      .withId('test1')
+      .withText('hi')
+      .authoredBy('Alice')
+      .publishedAt(new Date('2023-06-27T08:30:00.000Z'))
+      .build())
+
+  });
+
+  test("getAllOfUser() returns the message by id", async () => {
+    const messageRepository = new PrismaMessageRepository(prismaClient);
+
+    const startingMessages = [
+      messageBuilder()
+        .withId('test1')
+        .withText('hi')
+        .authoredBy('Alice')
+        .publishedAt(new Date('2023-06-27T08:30:00.000Z'))
+        .build(),
+      messageBuilder()
+        .withId('test2')
+        .withText('my name is Alice')
+        .authoredBy('Alice')
+        .publishedAt(new Date('2023-06-27T08:31:00.000Z'))
+        .build(),
+      messageBuilder()
+        .withId('test3')
+        .withText('Hi well I am Bob')
+        .authoredBy('Bob')
+        .publishedAt(new Date('2023-06-27T08:32:00.000Z'))
+        .build()
+    ]
+
+    await prismaClient.user.create({ data: { name: 'Alice' } })
+    await prismaClient.user.create({ data: { name: 'Bob' } })
+
+
+    for (const msg of startingMessages) {
+      const messageData = msg.data;
+      await prismaClient.message.create({
+        data: {
+          id: messageData.id,
+          authorId: messageData.author,
+          text: messageData.text,
+          publishedAt: messageData.publishedAt
+        }
+      })
+    }
+
+    const aliceMessages = await messageRepository.getAllOfUser('Alice');
+
+    expect(aliceMessages).toHaveLength(2);
+    expect(aliceMessages).toEqual(expect.arrayContaining([messageBuilder()
+      .withId('test1')
+      .withText('hi')
+      .authoredBy('Alice')
+      .publishedAt(new Date('2023-06-27T08:30:00.000Z'))
+      .build(),
+    messageBuilder()
+      .withId('test2')
+      .withText('my name is Alice')
+      .authoredBy('Alice')
+      .publishedAt(new Date('2023-06-27T08:31:00.000Z'))
+      .build()
+    ]))
+
+  });
+
+
+})
